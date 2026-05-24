e4bb20c5e44bb32680f0170d394df139b553fc77
Chris <julie@marks-MBP.lan>
2026-05-24 16:49:56 -0400

    minor __init__.py changes

--- src/bolum/__init__.py
+++ src/bolum/__init__.py
@@ -1,10 +1,13 @@
 '''
 Bolide Luminosity and Fragmentation Model
+Version 10
+Author: C.J. Miko
+This code is UNCLASSIFIED
 '''
 
 __version__ = "10.0.0"
+__author__ = "C.J. Miko"
 
 from .core import bolide_luminosity_model
-from .io import rdinput
 
-__all__ = ["bolide_luminosity_model", "rdinput"]
+__all__ = ["bolide_luminosity_model"]
